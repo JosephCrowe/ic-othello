574d51f13a96809a8a42fab10588d69cb1bbbb99
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-11-30 18:20:17 +0000

    Errors in get and set Piece

--- Othello.ml
+++ Othello.ml
@@ -1,10 +1,30 @@
-type piece = EMPTY | BLACK | WHITE
+type colour = BLACK | WHITE
+type piece = colour option
 
 class board size =
     object (self)
         initializer
             if size <= 2 || size mod 2 = 0 then
                 invalid_arg "Size must be greater than 2 and even."
-        val pieces = Array.make_matrix size size EMPTY
-	end
+        val pieces = Array.make_matrix size size None
+        
+        method setPiece x y givenPiece =
+            pieces.(x).(y) <- givenPiece
+        
+        method getPiece x y =
+            pieces.(x).(y)
+        
+    end
+
+
+
+class player name givenColour =
+    object (self)        
+        method getColour =
+            givenColour
+        
+        method getName =
+            name
+    end
+
 
