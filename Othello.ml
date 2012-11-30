767c8ecd5dead7208b96d8d1f64d82fd1257e089
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-11-30 17:07:59 +0000

    modulo correction

--- Othello.ml
+++ Othello.ml
@@ -1,7 +1,7 @@
 type piece = EMPTY | BLACK | WHITE
 
 class board size =
-	if (size <= 2 || size%2 != 0)
+	if (size <= 2 || (size mod 2) != 0)
 		then invalid_arg "Size must be greater than 2 and even.";
 	object(self)
 		val pieces = Array.make_matrix size size EMPTY
