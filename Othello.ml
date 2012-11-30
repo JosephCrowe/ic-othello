cbab7a7036e308586e33ed7a198544df9d72ffa2
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-11-30 17:00:48 +0000

    gksrhlre

---
+++ Othello.ml
@@ -0,0 +1,11 @@
+type piece = EMPTY | BLACK | WHITE
+
+class board size =
+	if (size <= 2 || size%2 != 0)
+		then invalid_arg "Size must be greater than 2 and even.";
+	object(self)
+		val pieces = Array.make_matrix size size EMPTY
+		
+	end
+	
+
