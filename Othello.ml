c2ca1304127d0657570da0edcfc233e2ad39ac1c
Harsh Ostwal <ostwal@ubuntu.(none)>
2012-11-30 18:55:27 +0000

    Shakespeare

--- Othello.ml
+++ Othello.ml
@@ -3,9 +3,10 @@ type piece = colour option
 
 class board (size : int) =
     object (self)
+    
         val pieces : piece array array
             = Array.make_matrix size size None
-
+            
         initializer
             if size <= 2 || size mod 2 = 0 then
                 invalid_arg "Size must be greater than 2 and even."
@@ -15,17 +16,28 @@ class board (size : int) =
 
         method getPiece x y =
             pieces.(x).(y)
+            
     end
 
 
 
-class player (name : string) (givenColour : colour) =
+class virtual player (name : string) (givenColour : colour) =
     object (self)        
         method getColour =
             givenColour
 
         method getName =
             name
+            
+        method virtual getMove : (int , int)
     end
 
-
+class humanPlayer (name : string) (givenColour : colour) =
+    object(self)
+        inherit player name givenColour
+        
+        method getMove =
+            print_string (name ^ ", enter your move as 'row, column' : ");
+            let inputs = read_line ();
+                    
+    end
