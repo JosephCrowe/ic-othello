18501f679b31d75777ad3636f6d2d11c56c44399
Joseph Crowe <jjc311@ic.ac.uk>
2012-11-30 18:27:27 +0000

    Cucumber

--- Othello.ml
+++ Othello.ml
@@ -3,26 +3,27 @@ type piece = colour option
 
 class board size =
     object (self)
+        val pieces : piece array array
+            = Array.make_matrix size size None
+
         initializer
             if size <= 2 || size mod 2 = 0 then
                 invalid_arg "Size must be greater than 2 and even."
-        val pieces = Array.make_matrix size size None
-        
+
         method setPiece x y givenPiece =
             pieces.(x).(y) <- givenPiece
-        
+
         method getPiece x y =
             pieces.(x).(y)
-        
     end
 
 
 
-class player name givenColour =
+class player (name : string) (givenColour : colour) =
     object (self)        
         method getColour =
             givenColour
-        
+
         method getName =
             name
     end
